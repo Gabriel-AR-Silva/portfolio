2d9ff32e2368aaec6b591948e1411f0c967ff04f
Gabriel Augusto Ribeiro da Silva <117488579+Gabriel-AR-Silva@users.noreply.github.com>
2023-12-02 20:32:51 +0000

    form ctt

---
+++ contato.php
@@ -0,0 +1,28 @@
+<?php
+
+if( $_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['submit'])) 
+{
+    $nome = filter_input(INPUT_POST,'name', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+    $email = filter_input(INPUT_POST,'email', FILTER_SANITIZE_EMAIL);
+    $subject = filter_input(INPUT_POST,'subject', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+    $description = filter_input(INPUT_POST,'description', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+
+    $to = 'gabrielsilva.webdeveloper@gmail.com';
+
+    $body = "Nome: $nome \n\r Email: $email \n\r Conteúdo: $description";
+
+    $head = "From: $email \n\r Reply-to: $to \n\r X=Mailer:PHP/" . phpversion();
+
+    if(mail($to, $subject, $body, $head)) {
+        header("location: ./index.html");
+        exit();
+    } else {
+        header("location: ./index.html");
+        exit();
+    }
+}
+
+header("location: ./index.html");
+exit();
+
+    
